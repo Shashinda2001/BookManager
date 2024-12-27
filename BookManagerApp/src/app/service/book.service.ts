30d371b215d0152892b0fe965aaf192f97f50d3f
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 18:44:54 +0530

    add CRUD methods in BookService for managing book operations

--- BookManagerApp/src/app/service/book.service.ts
+++ BookManagerApp/src/app/service/book.service.ts
@@ -14,4 +14,28 @@ export class BookService {
   getBooksList(): Observable <Book[]>{
     return this.httpClient.get<Book[]>(`${this.baseURL}`);
   }
+
+  //GET/BOOK/ID
+  getBookById(id: number): Observable<Book> {
+    return this.httpClient.get<Book>(`${this.baseURL}/${id}`);
+  }
+
+  //POST/BOOK
+  addBook(book: Book): Observable<Book> {
+    return this.httpClient.post<Book>(`${this.baseURL}`, book);
+  }
+  
+  //UPDATE/BOOK
+  updateBook(id: number, book: Book): Observable<void> {
+    return this.httpClient.put<void>(`${this.baseURL}/${id}`, book);
+  }
+
+  //DELETE/BOOK
+  deleteBook(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.baseURL}/${id}`);
+  }
+  
+  
+  
+  
 }
