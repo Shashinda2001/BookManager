8b5cb0ddbda42935b13ca96ce7c695c1093c3b41
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 21:28:34 +0530

    Refactor Book class constructor to provide default values for optional parameters

--- BookManagerApp/src/app/models/book.ts
+++ BookManagerApp/src/app/models/book.ts
@@ -6,11 +6,17 @@ export class Book {
     isbn: string;
     publicationDate: Date;
 
-    constructor(id: number, title: string, author: string, isbn: string, publicationDate: Date) {
+    constructor( id: number = 0,
+    title: string = '',
+    author: string = '',
+    isbn: string = '',
+    publicationDate: Date = new Date()) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.publicationDate = publicationDate;
     }
+
+    
 }
