2d801be4c0c0ffc66895227c3ab0a648ac2bf55d
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 15:17:43 +0530

    Create DbContext class for the application and add initial migration to set up the database schema

---
+++ BookManagerAPI/BookManagerAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagerAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
