1035226cffb6971ecb2d2c7baca3b18e8db76c73
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 18:39:38 +0530

    configure CORS to allow all origins, methods, and headers

--- BookManagerAPI/BookManagerAPI/Program.cs
+++ BookManagerAPI/BookManagerAPI/Program.cs
@@ -13,6 +13,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +31,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
