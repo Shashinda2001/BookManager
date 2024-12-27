8e0da017c8e72f7138c81bf4664b5fd382276d57
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 21:30:08 +0530

    Enhance CreateBookComponent styling with improved CSS for a better user experience

--- BookManagerApp/src/app/app.module.ts
+++ BookManagerApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
@@ -15,6 +15,7 @@ import { CreateBookComponent } from './create-book/create-book.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- BookManagerApp/src/app/create-book/create-book.component.ts
+++ BookManagerApp/src/app/create-book/create-book.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import {Book} from '../models/book';
+import { BookService } from '../service/book.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-book',
   templateUrl: './create-book.component.html',
   styleUrl: './create-book.component.css'
 })
-export class CreateBookComponent {
+export class CreateBookComponent implements OnInit {
+
+  book: Book = new Book(); 
+  ngOnInit(): void {
+      
+  }
+  constructor(private bookService: BookService ,
+    private router : Router
+  ){}
+
+  addBook(){
+    this.bookService.addBook(this.book).subscribe(data=>{
+      console.log(data);
+      this.goToBookList();
+    },
+  error => console.log(error));
+  
+  }
+
+  goToBookList(){
+    this.router.navigate(['/books']);
+  }
+
+
+  onSubmit(){
+    
+   this. addBook();
+  
+  }
 
 }
