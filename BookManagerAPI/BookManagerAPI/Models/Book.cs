c72a3649323985c5e593f4eb5a4c69e3b84913e7
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 15:12:45 +0530

    Initial commit

---
+++ BookManagerAPI/BookManagerAPI/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookManagerAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
+        public string Title { get; set; } = "";
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Author { get; set; } = "";
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [RegularExpression(@"^\d{3}-\d{10}$")]
+        public string ISBN { get; set; } = "";
+
+        [Required]
+        public DateTime PublicationDate { get; set; }
+    }
+}
