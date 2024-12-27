87ea080e37a6b401cd63c2884d7a098e9f5c376d
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 23:27:35 +0530

    Refactored BookController to use an empty in-memory list for storing books and implemented dynamic ID assignment.

--- BookManagerAPI/BookManagerAPI/Controllers/BookController.cs
+++ BookManagerAPI/BookManagerAPI/Controllers/BookController.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using BookManagerAPI.Data;
 using BookManagerAPI.Models;
 
 namespace BookManagerAPI.Controllers
@@ -14,61 +12,45 @@ namespace BookManagerAPI.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-
-        public BookController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        // Empty in-memory list to store books
+        private static List<Book> books = new List<Book>();
 
         // GET: api/Book
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        public ActionResult<IEnumerable<Book>> GetBooks()
         {
-            return await _context.Books.ToListAsync();
+            return Ok(books);
         }
 
         // GET: api/Book/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(int id)
+        public ActionResult<Book> GetBook(int id)
         {
-            var book = await _context.Books.FindAsync(id);
+            var book = books.FirstOrDefault(b => b.Id == id);
 
             if (book == null)
             {
                 return NotFound();
             }
 
-            return book;
+            return Ok(book);
         }
 
         // PUT: api/Book/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutBook(int id, Book book)
+        public IActionResult PutBook(int id, Book book)
         {
-            if (id != book.Id)
+            var existingBook = books.FirstOrDefault(b => b.Id == id);
+            if (existingBook == null)
             {
-                return BadRequest();
+                return NotFound();
             }
 
-            _context.Entry(book).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BookExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            existingBook.Title = book.Title;
+            existingBook.Author = book.Author;
+            existingBook.ISBN = book.ISBN;
+            existingBook.PublicationDate = book.PublicationDate;
 
             return NoContent();
         }
@@ -76,33 +58,25 @@ namespace BookManagerAPI.Controllers
         // POST: api/Book
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Book>> PostBook(Book book)
+        public ActionResult<Book> PostBook(Book book)
         {
-            _context.Books.Add(book);
-            await _context.SaveChangesAsync();
-
+            book.Id = books.Count == 0 ? 1 : books.Max(b => b.Id) + 1; // Assign new ID
+            books.Add(book);
             return CreatedAtAction("GetBook", new { id = book.Id }, book);
         }
 
         // DELETE: api/Book/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBook(int id)
+        public IActionResult DeleteBook(int id)
         {
-            var book = await _context.Books.FindAsync(id);
+            var book = books.FirstOrDefault(b => b.Id == id);
             if (book == null)
             {
                 return NotFound();
             }
 
-            _context.Books.Remove(book);
-            await _context.SaveChangesAsync();
-
+            books.Remove(book);
             return NoContent();
-        }
-
-        private bool BookExists(int id)
-        {
-            return _context.Books.Any(e => e.Id == id);
         }
     }
 }
