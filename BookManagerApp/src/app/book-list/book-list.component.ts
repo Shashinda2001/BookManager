491c9d3ec3a4a7ccf7c10641444024ad6d844d2b
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-27 18:41:37 +0530

    implement book fetching logic in ngOnInit with error handling

--- BookManagerApp/src/app/book-list/book-list.component.ts
+++ BookManagerApp/src/app/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import {Book} from '../models/book';
+import { BookService } from '../service/book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -10,26 +11,21 @@ export class BookListComponent implements OnInit {
 
   books : Book[] | undefined;
 
-  constructor(){}
+  constructor(private bookService : BookService ){}
 
   ngOnInit(): void {
-
-    this.books = [
-      {
-        id:12,
-        title: 'The Great Gatsby',
-        author: 'F. Scott Fitzgerald',
-        isbn: '978-1234567890',
-        publicationDate: new Date('1925-04-10') // Convert the string to a Date object
-      },
-      {
-        id:13,
-        title: '1984',
-        author: 'George Orwell',
-        isbn: '978-0451524935',
-        publicationDate: new Date('1949-06-08') // Another example with a Date
-      }
-    ];
+ this.getBooks();
       
+  }
+
+  private getBooks() {
+    this.bookService.getBooksList().subscribe({
+      next: (data) => {
+        this.books = data;
+      },
+      error: (err) => {
+        console.error('Error fetching books:', err);
+      }
+    });
   }
 }
